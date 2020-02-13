234de75ec01f88888dd1ec109f4862aff61091a6
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-02-12 21:26:20 -0700

    Add color/time script and style

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-var now = moment().format('dddd, MMM Do, YYYY');
+var now = moment().format('dddd, MMMM Do, YYYY');
 $('#currentDay').text(now);
+
+$('textarea').each(function() {
+    $(this)
+        .removeClass('present')
+        .removeClass('past')
+        .removeClass('future');
+
+    var test = moment().format('H');
+    var htmlData = $(this).attr('data-num');
+    console.log(htmlData);
+
+    if (test === htmlData) {
+        $(this).addClass('present');
+    } else if (test > htmlData) {
+        $(this).addClass('past');
+    } else {
+        $(this).addClass('future');
+    }
+});
 
 var storedArray = [
     {
@@ -42,35 +61,33 @@ var storedArray = [
 
 var userText = '';
 
-// $(document).ready(function() {
-// local storage functions
-function storage() {
-    var storedInputs = JSON.parse(localStorage.getItem('storedArray'));
-    if (storedInputs !== null) {
-        storedArray = storedInputs;
+$(document).ready(function() {
+    // local storage functions
+    function storage() {
+        var storedInputs = JSON.parse(localStorage.getItem('storedArray'));
+        if (storedInputs !== null) {
+            storedArray = storedInputs;
+        }
     }
-}
+    // for colors
 
-function storeUserInput() {
-    localStorage.setItem('storedArray', JSON.stringify(storedArray));
-}
-
-// show stored user input in text fields
-function insertStorage() {
-    for (let i = 0; i < storedArray.length; i++) {
-        var text = storedArray[i].todo;
-        var timeSlot = storedArray[i].time;
-        $(timeSlot).attr('placeholder', text);
+    function storeUserInput() {
+        localStorage.setItem('storedArray', JSON.stringify(storedArray));
     }
-}
-// save button calls
-$('.input-group').on('click', 'button', function(event) {
-    console.log(storedArray);
-    event.preventDefault();
-    for (let j = 0; j < storedArray.length; j++) {
-        var btnSelect = storedArray[j].time;
-        var btnId = this.id;
-        if ($('btnId:contains(btnSelect)')) {
+
+    // show stored user input in text fields
+    function insertStorage() {
+        for (let i = 0; i < storedArray.length; i++) {
+            var text = storedArray[i].todo;
+            var timeSlot = storedArray[i].time;
+            $(timeSlot).text(text);
+        }
+    }
+    // save button calls
+    $('.input-group').on('click', 'button', function(event) {
+        event.preventDefault();
+        for (let j = 0; j < storedArray.length; j++) {
+            var btnSelect = storedArray[j].time;
             var userText = $(btnSelect).val();
             if (userText !== '') {
                 storedArray[j].todo = userText;
@@ -78,10 +95,9 @@ $('.input-group').on('click', 'button', function(event) {
                 insertStorage();
             }
         }
-    }
-});
+    });
 
-// call startup functions
-storage();
-insertStorage();
-// });
+    // call startup functions
+    storage();
+    insertStorage();
+});
